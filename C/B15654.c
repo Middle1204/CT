59898c54dd94cf85f99f919b809153d265664b5e
Middle1204 <guu0414a@gmail.com>
2024-04-29 00:29:08 +0900

    C :: boj 2161

--- C/B15654.c
+++ C/B15654.c
@@ -1,38 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int N, M;
-int arr[8];
-int result[8];
+int n, m;
+int arr[10];
+int test[10];
+int print[10];
 
-int cmp(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
+int compare(const void *a, const void *b)
+{
+    int num1 = *(int *)a;
+    int num2 = *(int *)b;
+    if (num1 < num2)
+        return -1;
 
-void sq(int idx, int selected) {
-    if (selected == M) {
-        for (int i = 0; i < M; i++) {
-            printf("%d ", result[i]);
-        }
-        printf("\n");
-        return;
-    }
-
-    for (int i = idx; i < N; i++) {
-        result[selected] = arr[i];
-        sq(i + 1, selected + 1);
-    }
-}
-
-int main() {
-    scanf("%d %d", &N, &M);
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-
-    qsort(arr, N, sizeof(int), cmp);
-    sq(0, 0);
+    if (num1 > num2)
+        return 1;
 
     return 0;
+}
+
+void bt(int depth)
+{
+	int i;
+	if (m == depth)
+	{
+		i = 0;
+		while (i < m)
+		{
+			printf("%d ",print[i]);
+			i++;
+		}
+		printf("\n");
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			if (test[i] == 0)
+			{
+				test[i] = 1;
+				print[depth] = arr[i];
+				bt(depth + 1);
+				test[i] = 0;
+			}
+			i++;
+		}
+	}
+}
+
+int main()
+{
+	int i;
+	scanf("%d %d", &n, &m);
+	i = 0;
+	while(i < n)
+	{
+		scanf("%d", &arr[i]);
+		i++;
+	}
+	qsort(arr, n, sizeof(int), compare);
+	bt(0);
 }
