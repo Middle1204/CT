19c484b2195789d6dfe2bdc18e057f3398b95c1f
Middle1204 <guu0414a@gmail.com>
2024-04-30 16:59:54 +0900

    C :: boj 2529

--- C/B1015.c
+++ C/B1015.c
@@ -7,7 +7,7 @@ int main() {
         scanf("%d", &A[i]);
         P[i] = i;
     }
-    // Bubble sort를 이용하여 A[i]와 P[i]를 동시에 정렬
+//bubble sort
     for(int i = 0; i < N-1; i++) {
         for(int j = 0; j < N-1-i; j++) {
             if(A[j] > A[j+1]) {
@@ -20,11 +20,11 @@ int main() {
             }
         }
     }
-    // P배열을 이용하여 B배열 생성
+
     for(int i = 0; i < N; i++) {
         B[P[i]] = i;
     }
-    // B배열 출력
+
     for(int i = 0; i < N; i++) {
         printf("%d ", B[i]);
     }
