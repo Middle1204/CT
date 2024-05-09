bb5bb5ea7547e781ff729f7acc4411985c8a741f
Middle1204 <guu0414a@gmail.com>
2024-05-09 16:59:08 +0900

    C :: boj 1676

---
+++ C/B1676.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int N, count = 0;
+    scanf("%d", &N);
+
+    for(int i = 5; i <= N; i *= 5) {
+        count += N / i;
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
